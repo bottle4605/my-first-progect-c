67ad1e00ef61f8008ddb0526c39379b87b3b9e03
bottle4605 <2239026355@qq.com>
2026-07-07 18:26:53 +0800

    指针学习

---
+++ example/HelloWorld/HelloWorld/05_pointer_demo5.c
@@ -0,0 +1,18 @@
+//#include<stdio.h>
+//
+//int* method();
+//int main()
+//{
+//	//调用method函数，并使用该函数中的变量a
+//	int* p = method();
+//	
+//	printf("%d\n", *p);
+//
+//	return 0;
+//}
+//
+//int* method()
+//{
+//	static int a = 10;
+//	return &a;
+//}
