10a5e49ca1351f6f409d7103f46d2fa97178dec1
bottle4605 <2239026355@qq.com>
2026-07-17 16:59:06 +0800

    结构体练习

---
+++ 01_The_C_Programming_Language/10_exercise_1-6.c
@@ -0,0 +1,6 @@
+//#include<stdio.h>
+//int main(void)
+//{
+//	printf("value of expression:%d", getchar() != EOF);
+//	return 0;
+//}
---
+++ 01_The_C_Programming_Language/12_copy_1.5.c
@@ -0,0 +1,10 @@
+//#include<stdio.h>
+////count characters in input; 1st version
+//main()
+//{
+//	long nc;
+//	nc = 0;
+//	while (getchar() != EOF)
+//		++nc;	//nc = nc + 1 µÈÓÚ ++nc
+//	printf("%ld\n", nc);
+//}
